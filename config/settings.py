7b700ff33ede9e5b5603974433505f93b92aec36
Siddharth Das <66112421+eyepatch-3097@users.noreply.github.com>
2026-02-18 18:14:03 +0530

    Add sidebar_recent_workspaces context processor

--- config/settings.py
+++ config/settings.py
@@ -96,6 +96,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'billing.context_processors.billing_summary',
+                'workspaces.context_processors.sidebar_recent_workspaces',
             ],
         },
     },
