f054703169b6cf1d3f91ba42f1680b98121de24e
Siddharth Das <66112421+eyepatch-3097@users.noreply.github.com>
2026-03-04 00:42:34 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -69,6 +69,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
     'accounts',
     'workspaces',
     'cms',
